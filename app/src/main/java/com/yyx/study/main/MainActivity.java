09b2d3843cf56def8faa34226009d3e136820bda
yyx <775696065@qq.com>
2017-07-26 14:47:24 +0800

    权限请求流程

--- app/src/main/java/com/yyx/study/main/MainActivity.java
+++ app/src/main/java/com/yyx/study/main/MainActivity.java
@@ -1,9 +1,15 @@
 package com.yyx.study.main;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.text.Editable;
 import android.text.Spannable;
 import android.text.SpannableString;
@@ -15,6 +21,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.yyx.study.R;
 import com.yyx.study.video.FullScreenPlayVideoActivity;
@@ -22,6 +29,7 @@ import com.yyx.study.video.VideoActivity;
 
 public class MainActivity extends Activity implements TextWatcher, View.OnKeyListener {
     private EditText etContent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,8 +47,68 @@ public class MainActivity extends Activity implements TextWatcher, View.OnKeyLis
         startActivity(new Intent(this, AutoLayoutActivity.class));
     }
 
+    /**
+     * 请求权限
+     *
+     * @param view
+     */
+    public void onRequestPermissions(View view) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            //有权限
+            doNext("相机权限已经开启");
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA);
+        }
+    }
+
+    private static final int CAMERA = 2;
+
+    private void doNext(String str) {
+        Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == CAMERA) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //有权限
+                doNext("谢谢授权");
+            } else {
+                showExplainDialog();
+                // Permission Denied
+                Toast.makeText(MainActivity.this, "您没有授权该权限，请在设置中打开授权", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    /**
+     * 解释为什么要申请权限
+     */
+    private void showExplainDialog() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("再次申请权限")
+                    .setMessage("你拒绝我，没法玩啊！")
+                    .setPositiveButton("授权", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, CAMERA);
+                        }
+                    })
+                    .setNegativeButton("拒绝", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA);
+        }
+    }
 
 
+    //<------------------------------------edittext 分割数据------------------------------------------------>
     /**
      * 记录下上次的文本长度
      */
@@ -72,6 +140,7 @@ public class MainActivity extends Activity implements TextWatcher, View.OnKeyLis
             onSetSpan();
         }
     }
+
     /**
      * 核心步骤
      * 主要通过SpannableString来实现标签分组
@@ -111,13 +180,13 @@ public class MainActivity extends Activity implements TextWatcher, View.OnKeyLis
                 && event.getAction() == KeyEvent.ACTION_DOWN) {
             if (content.length() > 0) {
                 String last = content.substring(content.length() - 1, content.length());
-                if(!" ".equals(last)){
+                if (!" ".equals(last)) {
                     return false;
                 }
                 String[] m = content.split(" ");
                 String lastTag = m[m.length - 1];
                 //content.length()-1 为了去除空格
-                content = content.substring(0, content.length()-1 - lastTag.length());
+                content = content.substring(0, content.length() - 1 - lastTag.length());
                 etContent.setText(content);
                 etContent.setSelection(content.length());
                 return true;
